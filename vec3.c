7b37ba40c3a2c22138ee47c5c83f4b0206808fed
gorgemul <zeyang793@gmail.com>
2025-10-01 13:48:34 +0800

    setup camera and ray

--- vec3.c
+++ vec3.c
@@ -1,21 +1,115 @@
 #include "vec3.h"
+#include <stddef.h>
+#include <stdarg.h>
+#include <math.h>
 
-Vec3 vec3_add(Vec3 *vec1, Vec3 *vec2)
+Vec3 vec3_add(size_t n, Vec3 *vec, ...)
 {
-    return (Vec3){vec1->x + vec2->x, vec1->y + vec2->y, vec1->z + vec2->z};
+    Vec3 result_vec = { vec->x, vec->y, vec->z };
+    if (n == 0) return result_vec;
+    va_list args;
+    va_start(args, vec);
+    for (size_t i = 0; i < n; ++i) {
+        Vec3 *current = va_arg(args, Vec3*);
+        result_vec.x += current->x;
+        result_vec.y += current->y;
+        result_vec.z += current->z;
+    }
+    va_end(args);
+    return result_vec;
 }
 
-Vec3 vec3_sub(Vec3 *vec1, Vec3 *vec2)
+Vec3 vec3_sub(size_t n, Vec3 *vec, ...)
 {
-    return (Vec3){vec1->x - vec2->x, vec1->y - vec2->y, vec1->z - vec2->z};
+    Vec3 result_vec = { vec->x, vec->y, vec->z };
+    if (n == 0) return result_vec;
+    va_list args;
+    va_start(args, vec);
+    for (size_t i = 0; i < n; ++i) {
+        Vec3 *current = va_arg(args, Vec3*);
+        result_vec.x -= current->x;
+        result_vec.y -= current->y;
+        result_vec.z -= current->z;
+    }
+    va_end(args);
+    return result_vec;
 }
 
-Vec3 vec3_mul(Vec3 *vec1, Vec3 *vec2)
+Vec3 vec3_mul(size_t n, Vec3 *vec, ...)
 {
-    return (Vec3){vec1->x * vec2->x, vec1->y * vec2->y, vec1->z * vec2->z};
+    Vec3 result_vec = { vec->x, vec->y, vec->z };
+    if (n == 0) return result_vec;
+    va_list args;
+    va_start(args, vec);
+    for (size_t i = 0; i < n; ++i) {
+        Vec3 *current = va_arg(args, Vec3*);
+        result_vec.x *= current->x;
+        result_vec.y *= current->y;
+        result_vec.z *= current->z;
+    }
+    va_end(args);
+    return result_vec;
 }
 
-Vec3 vec3_div(Vec3 *vec1, Vec3 *vec2)
+Vec3 vec3_div(size_t n, Vec3 *vec, ...)
 {
-    return (Vec3){vec1->x / vec2->x, vec1->y / vec2->y, vec1->z / vec2->z};
+    Vec3 result_vec = { vec->x, vec->y, vec->z };
+    if (n == 0) return result_vec;
+    va_list args;
+    va_start(args, vec);
+    for (size_t i = 0; i < n; ++i) {
+        Vec3 *current = va_arg(args, Vec3*);
+        result_vec.x /= current->x;
+        result_vec.y /= current->y;
+        result_vec.z /= current->z;
+    }
+    va_end(args);
+    return result_vec;
+}
+
+Vec3 vec3_mul_scalar(size_t n, Vec3 *vec, ...)
+{
+    Vec3 result_vec = { vec->x, vec->y, vec->z };
+    if (n == 0) return result_vec;
+    va_list args;
+    va_start(args, vec);
+    for (size_t i = 0; i < n; ++i) {
+        double scalar = va_arg(args, double);
+        result_vec.x *= scalar;
+        result_vec.y *= scalar;
+        result_vec.z *= scalar;
+    }
+    va_end(args);
+    return result_vec;
+}
+
+Vec3 vec3_div_scalar(size_t n, Vec3 *vec, ...)
+{
+    Vec3 result_vec = { vec->x, vec->y, vec->z };
+    if (n == 0) return result_vec;
+    va_list args;
+    va_start(args, vec);
+    for (size_t i = 0; i < n; ++i) {
+        double scalar = va_arg(args, double);
+        result_vec.x /= scalar;
+        result_vec.y /= scalar;
+        result_vec.z /= scalar;
+    }
+    va_end(args);
+    return result_vec;
+}
+
+Vec3 vec3_unit_vector(Vec3 *vec)
+{
+    return vec3_div_scalar(1, vec, vec3_len(vec));
+}
+
+double vec3_len_squared(Vec3 *vec)
+{
+    return vec->x * vec->x + vec->y * vec->y + vec->z * vec->z;
+}
+
+double vec3_len(Vec3 *vec)
+{
+    return sqrt(vec3_len_squared(vec));
 }
--- vec3.h
+++ vec3.h
@@ -1,13 +1,22 @@
 #ifndef VEC3_H_
 #define VEC3_H_
 
+#include <stddef.h>
+
 typedef struct {
     double x, y, z;
 } Vec3;
 
-Vec3 vec3_add(Vec3 *vec1, Vec3 *vec2);
-Vec3 vec3_sub(Vec3 *vec1, Vec3 *vec2);
-Vec3 vec3_mul(Vec3 *vec1, Vec3 *vec2);
-Vec3 vec3_div(Vec3 *vec1, Vec3 *vec2);
+typedef Vec3 Point3;
+
+Vec3 vec3_add(size_t n, Vec3 *vec, ...);
+Vec3 vec3_sub(size_t n, Vec3 *vec, ...);
+Vec3 vec3_mul(size_t n, Vec3 *vec, ...);
+Vec3 vec3_div(size_t n, Vec3 *vec, ...);
+Vec3 vec3_mul_scalar(size_t n, Vec3 *vec, ...);
+Vec3 vec3_div_scalar(size_t n, Vec3 *vec, ...);
+Vec3 vec3_unit_vector(Vec3 *vec);
+double vec3_len_squared(Vec3 *vec);
+double vec3_len(Vec3 *vec);
 
 #endif // VEC3_H_
